2d17779669b678f51ef69b6ff0e9ddad05abada7
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2020-01-18 17:32:14 -0300

    #CHANGES:Limpeza de formulario

--- api_backend/src/controller/CardController.js
+++ api_backend/src/controller/CardController.js
@@ -3,16 +3,16 @@ const express = require('express');
 
 
 module.exports = {
+
     async index(req,res){
-        console.log('oi')
         const response = await Card.find();
         return res.json(response);
     },
+
     async store(req,res){
         const {title,description,value,date} = req.body;
 
-        const parsedDate = new Date(date);
-        console.log(req.body);
+
         const card = await Card.create({
             title,
             date,
--- frontend/src/components/CardSubmit/Card.js
+++ frontend/src/components/CardSubmit/Card.js
@@ -12,34 +12,43 @@ function Card(){
     const[description,setDescription] = useState('');
     const[value,setValue] = useState(0);
 
-     function insertCard(e){
+     async function insertCard(e){
         e.preventDefault();
+
        const insertObject = {
            title,
            date,
            description,
            value
        }
-        apiURL.post('/card',insertObject);
-       
-    }
+        
+       const response = await apiURL.post('/card',insertObject);
+
+        setTitle('');
+        setDate('');
+        setDescription('');
+        setValue(0);
+
+        return response;
+
+        }
     return (
         <>
         <form onSubmit={insertCard}>
             <div className="submitCard">
                 <h1 className="card-title">Preencha um gasto</h1>
                 <label>Titulo do gasto</label>
-                <input type="text" name="title" id="" onChange={e => setTitle(e.target.value)}/>
+                <input type="text" name="title" id="" value = {title} onChange={e => setTitle(e.target.value)}/>
                 <br/>
                 <label>Data do gasto</label>
-                <input type="date" name="date" id="" onChange={e => setDate(e.target.value)}/>
+                <input type="date" name="date" id="" value={date} onChange={e => setDate(e.target.value)}/>
                 <br/>
                 <label>Descrição do gasto</label>
                 <br/>
-                <textarea name="description" id="" cols="30" rows="10" onChange={e => setDescription(e.target.value)}></textarea>
+                <textarea name="description" id="" cols="30" rows="10" value={description} onChange={e => setDescription(e.target.value)}></textarea>
                 <br/>
                 <label>Valor do gasto</label>
-                <input type="number" name="value" onChange={e => setValue(e.target.value)}/>
+                <input type="number" name="value" value = {value} onChange={e => setValue(e.target.value)}/>
                 <br/>
                 <input type="submit" value="Registrar" className="sendB"/>
             </div>
